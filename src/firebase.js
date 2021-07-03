b23e978107bf62ca57d5ea3eeb9630956c57af67
nikhjha <nikh.jha550@gmail.com>
2021-07-03 15:14:06 +0530

    Debug production error 2

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,14 @@ import "firebase/auth";
 import "firebase/firebase-firestore";
 
 const firebaseConfig = {
-    apiKey: ""+process.env.REACT_APP_APIKEY,
-    authDomain: ""+process.env.REACT_APP_AUTHDOMAIN,
-    projectId: ""+process.env.REACT_APP_PROJECTID,
-    storageBucket: ""+process.env.REACT_APP_STORAGEBUCKET,
-    messagingSenderId: ""+process.env.REACT_APP_MESSAGINGSENDERID,
-    appId: ""+process.env.REACT_APP_APPID,
-    measurementId: ""+process.env.REACT_APP_MEASUREMENTID
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_PROJECTID,
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_APPID,
+    measurementId: process.env.REACT_APP_MEASUREMENTID
   };
-  console.log(firebaseConfig);
   // Initialize Firebase
   const app = firebase.initializeApp(firebaseConfig);
 
